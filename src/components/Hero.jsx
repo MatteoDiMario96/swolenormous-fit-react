edb8b577441a6b8f9170bf8a99e5401eda72727a
MatteoDiMario96 <dm.matteo96@live.it>
2024-12-10 19:52:18 +0100

    Create the section for the introduction

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero() {
                     <p className="uppercase">It's time to get</p>
                     <h1 className="uppercase font-semibold text-4xl sm:text-5xl md:text6xl lg:text-7xl">Swole<span className="text-blue-400">normous</span></h1>
                 </div>
-                <p>I hereby acknowledgement that I may become <span className="text-blue-400 font-semibold">unbelievably swolenormous</span> and accept all risks of becoming the local <span className="text-blue-400 font-semibold">mass montrosity</span>, afflicted with severe body dismorphia, unable to fit through doors.</p>
+                <p className="text-sm md:text-base font-light">I hereby acknowledgement that I may become <span className="text-blue-400 font-semibold">unbelievably swolenormous</span> and accept all risks of becoming the local <span className="text-blue-400 font-semibold">mass montrosity</span>, afflicted with severe body dismorphia, unable to fit through doors.</p>
                 <button className="bg-slate-950 border-blue-400 rounded-lg p-4 border-solid blueShadows duration-200">Accept & Begin</button>
             </div>
         </>
